dafd0f9f15c6cb4070eade0b172312382a9bccb7
JD2895 <j.davis.2895@gmail.com>
2021-05-15 23:47:42 -0400

    - started work on finish interaction - there is danger now (but it isnt fully implemented)

---
+++ Assets/CannonProjectileInteraction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonProjectileInteraction : MonoBehaviour
+{
+    bool isInteractable = false;
+
+    CircleCollider2D mainCollider;
+    Rigidbody2D rb;
+    MovementRecorder movementRecorder;
+    SideMovement sideMovement;
+    ArtificialGravity artificialGravity;
+
+    BasicMoveset controls;
+
+    private void Awake()
+    {
+        controls = new BasicMoveset();
+        controls.Debug.StartRecording.performed += _ => ChangeInteractionState(true);
+        controls.Debug.PlayRecording.performed += _ => ChangeInteractionState(false);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void ChangeInteractionState(bool isRecording)
+    {
+        isInteractable = !isRecording;
+    }
+
+    private void Start()
+    {
+        ChangeInteractionState(true);
+        mainCollider = this.GetComponent<CircleCollider2D>();
+        rb = this.GetComponent<Rigidbody2D>();
+        movementRecorder = this.GetComponentInChildren<MovementRecorder>();
+        sideMovement = this.GetComponent<SideMovement>();
+        artificialGravity = this.GetComponent<ArtificialGravity>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hazard") && isInteractable)
+        {
+            Debug.Log("DED");
+            rb.bodyType = RigidbodyType2D.Static;
+            mainCollider.enabled = false;
+            movementRecorder.enabled = false;
+            sideMovement.enabled = false;
+            artificialGravity.enabled = false;
+        }
+    }
+}
--- Assets/TileStyleSwitcher.cs
+++ Assets/TileStyleSwitcher.cs
@@ -7,5 +7,33 @@ public class TileStyleSwitcher : MonoBehaviour
     public GameObject fullTilemap;
     public GameObject bwTileMap;
 
+    BasicMoveset controls;
 
+    private void Awake()
+    {
+        controls = new BasicMoveset();
+        controls.Debug.StartRecording.performed += _ => ChangeTileStyle(true);
+        controls.Debug.PlayRecording.performed += _ => ChangeTileStyle(false);
+    }
+
+    private void Start()
+    {
+        ChangeTileStyle(true);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void ChangeTileStyle(bool isRecording)
+    {
+        bwTileMap.SetActive(isRecording);
+        fullTilemap.SetActive(!isRecording);
+    }
 }
